a1a6205c396a3dd6210b9df47eb5c3d558f42eb4
Luciano Scarpaci <tauxsik@gmail.com>
2023-10-13 15:13:04 -0400

    Update Talk

--- talk.py
+++ talk.py
@@ -1,7 +1,10 @@
 from gtts import gTTS
+import os
 
 text = "Hello, its nice to meet you! My name is Daisy. I am a robot. I am here to help you."
 language = 'en'
 
 tts = gTTS(text=text, lang=language, tld='com.au')
-tts.save('hello_daisy.wav')
+tts.save('hello_daisy.mp3')
+
+os.system("ffmpeg -i hello_daisy.mp3 -acodec pcm_s24le -ac 1 -ar 44100 daisy_signed.wav")
