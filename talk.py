f95713b14703fa9643513e01c00616430f429eb2
Luciano Scarpaci <tauxsik@gmail.com>
2023-10-14 22:35:53 -0400

    Update talk file

--- talk.py
+++ talk.py
@@ -12,3 +12,8 @@ tts.save(file_name+'.mp3')
 os.system("ffmpeg -i " + file_name + ".mp3" +
           " -acodec pcm_s24le -ac 1 -ar 44100 " + file_name + ".wav")
 os.system("rm " + file_name + ".mp3")
+
+
+# SSH into the robot dog
+os.system("sshpass -p 123 scp -r " + file_name +
+          ".wav unitree@192.168.123.13:/home/unitree")
