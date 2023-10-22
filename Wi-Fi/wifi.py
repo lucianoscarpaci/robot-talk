d75d4d7a3ed3779710afab07e05ed5542959c760
Luciano Scarpaci <tauxsik@gmail.com>
2023-10-22 17:04:14 -0400

    Masquerade nano traffic

--- Wi-Fi/wifi.py
+++ Wi-Fi/wifi.py
@@ -11,6 +11,15 @@ class WiFi:
         subprocess.run(
             ['sysctl', '-w', 'net.ipv4.ip_forward=1'])
 
+    def masquerade_nano_traffic(self):
+        subprocess.run(['iptables', '-t', 'nat', '-A',
+                       'POSTROUTING', '-o', 'wlan2', '-j', 'MASQUERADE'])
+        subprocess.run(['iptables', '-A', 'FORWARD', '-i',
+                       'eth0', '-o', 'wlan2', '-j', 'ACCEPT'])
+        subprocess.run(['iptables', '-A', 'FORWARD', '-i', 'wlan2', '-o', 'eth0',
+                       '-m', 'state', '--state', 'RELATED,ESTABLISHED', '-j', 'ACCEPT'])
+        subprocess.run(['ifconfig', 'wlan2', 'up'])
+
     def begin_stage1(self):
         print("\033[92mStage 1 is about to start...\033[0m")
         # Allow forwarding on the raspberry pi
@@ -21,6 +30,7 @@ class WiFi:
         else:
             print("\033[91mIP forwarding Failed.\033[0m")
         print("\033[92mPutting all nano traffic behind the Pi...\033[0m")
+        self.masquerade_nano_traffic()
 
     def begin_stage2(self):
         print("Stage 2 has been selected")
@@ -42,8 +52,10 @@ class WiFi:
 
             # Printing the ASCII art
             print("\033[94m" + dog_wifi_ascii + "\033[0m")
-            print("\033[94m+++++++++++++++++++++ Dog WiFi +++++++++++++++++++++\033[0m")
-            stage = input("\033[92mWhich WiFi stage are you in? (1 or 2):\033[0m")
+            print(
+                "\033[94m+++++++++++++++++++++ Dog WiFi +++++++++++++++++++++\033[0m")
+            stage = input(
+                "\033[92mWhich WiFi stage are you in? (1 or 2):\033[0m")
 
             if stage == "1":
                 self.begin_stage1()
@@ -55,7 +67,8 @@ class WiFi:
                 sys.exit()
             else:
                 print("\033[91mError: wrong input. Type '1' or '2' \033[0m")
-                print("\033[94m+++++++++++++++++++++ Dog WiFi +++++++++++++++++++++\033[0m")
+                print(
+                    "\033[94m+++++++++++++++++++++ Dog WiFi +++++++++++++++++++++\033[0m")
 
 
 wifi = WiFi()
